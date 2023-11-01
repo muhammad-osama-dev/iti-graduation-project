12d79282fe43c0940d12e51071c247711f817024
Muhammad Osama <thegame9555@gmail.com>
2023-11-01 19:23:12 +0200

    add bucket

--- storage/main.tf
+++ storage/main.tf
@@ -10,14 +10,22 @@ resource "google_artifact_registry_repository" "my-repo" {
 }
 
 
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
 
-# resource "google_artifact_registry_repository_iam_binding" "iam_binding" {
-#   repository = google_artifact_registry_repository.my-repo.name
-#   location   = var.region2
-#   role       = "roles/artifactregistry.reader"
-#   project    = var.project_id
-
-#   members = [
-#     "serviceAccount:${var.sa_email}"
-#   ]
-# }
+resource "google_storage_bucket" "tf-bucket" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
+  }
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
