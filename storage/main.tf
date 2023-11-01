f8fe1835264b9e3259eac07214855b1ba0c2cf85
Muhammad Osama <thegame9555@gmail.com>
2023-11-01 19:43:34 +0200

    add bucket

--- storage/main.tf
+++ storage/main.tf
@@ -10,6 +10,22 @@ resource "google_artifact_registry_repository" "my-repo" {
 }
 
 
+
+resource "google_kms_key_ring" "keyring" {
+  name     = "keyring-example"
+  location = "global"
+}
+
+resource "google_kms_crypto_key" "terraform_state_bucket" {
+  name            = "terraform_state_bucket"
+  key_ring        = google_kms_key_ring.keyring.id
+  rotation_period = "100000s"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
